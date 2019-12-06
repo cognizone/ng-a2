918dccca7acbf3e7b7a38c5cc289fefdb280cc22
William Chelman <william.chelman@cogni.zone>
2019-12-06 10:12:36 +0100

    chore: change structure to angular lib

---
+++ projects/cognizone/a2/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of a2
+ */
+
+export * from "./lib/a2-core";
+export * from "./lib/elasticsearch";
+export * from "./lib/precondition";
+export * from "./lib/restcall-service";
