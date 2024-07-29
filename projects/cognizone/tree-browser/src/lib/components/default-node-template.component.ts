ba9f559cdcf3dc624e18a962aac0340ac5f686df
marianna tselepi <marianna.tselepi@cogni.zone>
2024-07-29 21:41:45 +0200

    feat: add tree-browser

---
+++ projects/cognizone/tree-browser/src/lib/components/default-node-template.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { AbstractLimitIncreaseComponent, AbstractNodeTemplateComponent } from './abstract-node-template.component';
+
+@Component({
+  selector: 'tb-default-node-template',
+  template: `
+    <div *ngIf="nodeWrapper.isRoot()">
+      Root
+      <div style="margin-left: 10px">
+        <tb-internal-children-template [nodeWrapper]="nodeWrapper" [internalTemplate]="internalTemplate"></tb-internal-children-template>
+      </div>
+    </div>
+    <div *ngIf="!nodeWrapper.isRoot()">
+      Node #{{ nodeWrapper.index }}
+      <button *ngIf="nodeWrapper.hasChildren()" (click)="toggle()">{{ collapsed ? 'expand' : 'collapse' }}</button>
+
+      <div style="margin-left: 10px" *ngIf="!collapsed">
+        <tb-internal-children-template [nodeWrapper]="nodeWrapper" [internalTemplate]="internalTemplate"></tb-internal-children-template>
+      </div>
+    </div>
+  `,
+})
+export class DefaultNodeTemplateComponent extends AbstractNodeTemplateComponent<any> {
+  collapsed = true;
+
+  constructor(private ref: ChangeDetectorRef) {
+    super();
+  }
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+    this.ref.detectChanges();
+  }
+}
+
+@Component({
+  selector: 'tb-default-limit-increase',
+  template: ` <button (click)="clickAction()">See more</button> `,
+})
+export class DefaultLimitIncreaseComponent extends AbstractLimitIncreaseComponent {}
