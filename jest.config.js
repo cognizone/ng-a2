8da35d387a3a20654c501fea440b369d8591d2e1
IB <izabela.borkowska@cogni.zone>
2025-11-05 17:22:40 +0100

    update jest + jest config - test passed

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['./setupJest.ts'],
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-      astTransformers: ['jest-preset-angular/build/InlineFilesTransformer', 'jest-preset-angular/build/StripStylesTransformer'],
-    },
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.html$',
+      },
+    ],
   },
   modulePathIgnorePatterns: ['<rootDir>/dist/cognizone/a2/*', '<rootDir>/dist/cognizone/server-file-browser/*'],
   moduleNameMapper: {
@@ -18,3 +21,5 @@ module.exports = {
     '@cognizone/sse/(.*)': '<rootDir>/projects/cognizone/sse/src/public-api.ts',
   },
 };
+
+module.exports = config;
